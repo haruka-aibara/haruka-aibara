7058f12b15686253d1587d9e6284b3fee0b5e31d
haruharumolly <haruharumolly@outlook.com>
2024-08-30 00:18:14 +0900

    Modify handle_app_mention_events to extract user input

--- bedrock-slack-ai-chatbot/handle_app_mention_events.py
+++ bedrock-slack-ai-chatbot/handle_app_mention_events.py
@@ -1,6 +1,7 @@
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import os
+import re
 
 # アプリの初期化
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
@@ -8,7 +9,8 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 # Slackイベントハンドラー：Slackアプリがメンションされた時
 @app.event("app_mention")
 def handle_app_mention_events(event, say):
-    say(event["text"])
+    input_text = re.sub("<@.+>", "", event["text"]).strip()
+    say(input_text)
 
 # アプリを起動
 if __name__ == "__main__":
