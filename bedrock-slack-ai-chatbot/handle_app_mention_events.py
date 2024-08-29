a0f7f7bb83699589b74b4c99b8ba3677772b8094
haruharumolly <haruharumolly@outlook.com>
2024-08-30 08:40:19 +0900

    Configure Bedrock client with ap-northeast-1 region

--- bedrock-slack-ai-chatbot/handle_app_mention_events.py
+++ bedrock-slack-ai-chatbot/handle_app_mention_events.py
@@ -1,7 +1,43 @@
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+import boto3
+import json
 import os
 import re
+
+bedrock_runtime = boto3.client("bedrock-runtime", region_name="ap-northeast-1")
+
+# Bedrockを使って応答テキストを生成する
+def generate_answer(input_text):
+    # メッセージをセット
+    messages = [
+        {
+            "role": "user",
+            "content": input_text,
+        },
+    ]
+
+    # リクエストBODYをセット
+    request_body = json.dumps({
+        "messages": messages,
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 1000,
+    })
+
+    # Bedrock APIを呼び出す
+    response = bedrock_runtime.invoke_model(
+        modelId="anthropic.claude-3-haiku-20240307-v1:0",
+        accept="application/json",
+        contentType="application/json",
+        body=request_body,
+    )
+
+    # レスポンスBODYから応答テキストを取り出す
+    response_body = json.loads(response.get("body").read())
+    output_text = response_body.get("content")[0].get("text")
+
+    # 応答テキストを戻り値として返す
+    return output_text
 
 # アプリの初期化
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
@@ -10,7 +46,8 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 @app.event("app_mention")
 def handle_app_mention_events(event, say):
     input_text = re.sub("<@.+>", "", event["text"]).strip()
-    say(input_text)
+    output_text = generate_answer(input_text)
+    say(output_text)
 
 # アプリを起動
 if __name__ == "__main__":
