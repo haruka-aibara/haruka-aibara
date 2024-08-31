3d8048d23d5545ed711bf7bfb0f4bfdfe0a8f56f
haruharumolly <haruharumolly@outlook.com>
2024-08-31 12:30:33 +0900

    create bedrock backend lambda and sqs integration

--- bedrock-slack-ai-chatbot/main_lambda.tf
+++ bedrock-slack-ai-chatbot/main_lambda.tf
@@ -16,7 +16,7 @@ resource "aws_lambda_layer_version" "slack_bolt" {
 # Lambda関数のコードをzip化
 data "archive_file" "lambda_code" {
   type        = "zip"
-  source_dir  = "lambda_function"
+  source_dir  = "slack_bot_lambda"
   output_path = "archives/lambda_function.zip"
 }
 
@@ -116,4 +116,130 @@ data "aws_iam_policy_document" "lambda_sqs" {
 resource "aws_iam_role_policy_attachment" "lambda_sqs" {
   role       = aws_iam_role.slack_bolt_app_role.name
   policy_arn = aws_iam_policy.lambda_sqs.arn
+}
+
+# ===== Bedrock =====
+
+# Lambda関数のコードをzip化
+data "archive_file" "lambda_bedrock_code" {
+  type        = "zip"
+  source_dir  = "bedrock_backend_lambda"
+  output_path = "archives/bedrock_backend.zip"
+}
+
+# Bedrock Backend
+resource "aws_lambda_function" "slack_bolt_app_bedrock_backend" {
+  filename         = data.archive_file.lambda_bedrock_code.output_path
+  function_name    = "${local.app_name}-bedrock-backend"
+  handler          = "bedrock_backend.lambda_handler"
+  runtime          = "python3.12"
+  architectures    = ["x86_64"]
+  source_code_hash = data.archive_file.lambda_bedrock_code.output_base64sha256
+  role             = aws_iam_role.slack_bolt_app_bedrock_backend_role.arn
+  layers           = [aws_lambda_layer_version.slack_bolt.arn]
+  timeout          = 30
+
+  environment {
+    variables = {
+      SLACK_BOT_TOKEN = var.slack_bot_token
+      REGION          = data.aws_region.current.name
+    }
+  }
+}
+
+# IAMロールの定義
+resource "aws_iam_role" "slack_bolt_app_bedrock_backend_role" {
+  name               = "${local.app_name}-bedrock-backend-role"
+  path               = "/service-role/"
+  assume_role_policy = data.aws_iam_policy_document.lambda_bedrock_assume_role.json
+}
+
+# Lambda用のAssume Role Policyを定義するデータブロック
+data "aws_iam_policy_document" "lambda_bedrock_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+# カスタムポリシードキュメントの定義
+data "aws_iam_policy_document" "lambda_bedrock_backend" {
+  statement {
+    sid    = "sqs"
+    effect = "Allow"
+    actions = [
+      "sqs:GetQueueAttributes",
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage"
+    ]
+    resources = [
+      aws_sqs_queue.bedrock_backend.arn
+    ]
+  }
+
+  statement {
+    sid    = "bedrock"
+    effect = "Allow"
+    actions = [
+      "bedrock:InvokeModel"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+# IAMポリシーリソース
+resource "aws_iam_policy" "lambda_bedrock_backend" {
+  name   = "${local.app_name}-lambda-bedrock-backend-policy"
+  policy = data.aws_iam_policy_document.lambda_bedrock_backend.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_bedrock_backend" {
+  role       = aws_iam_role.slack_bolt_app_bedrock_backend_role.name
+  policy_arn = aws_iam_policy.lambda_bedrock_backend.arn
+}
+
+# IAMポリシーリソース
+resource "aws_iam_policy" "lambda_bedrock_logging" {
+  name   = "${local.app_name}-lambda-bedrock-logging-policy"
+  path   = "/service-role/"
+  policy = data.aws_iam_policy_document.lambda_bedrock_logging.json
+}
+
+# カスタムポリシードキュメントの定義
+data "aws_iam_policy_document" "lambda_bedrock_logging" {
+  statement {
+    sid       = "loggroup"
+    effect    = "Allow"
+    actions   = ["logs:CreateLogGroup"]
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
+  }
+
+  statement {
+    sid    = "logevents"
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${local.app_name}-bedrock-backend:*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_bedrock_logging" {
+  role       = aws_iam_role.slack_bolt_app_bedrock_backend_role.name
+  policy_arn = aws_iam_policy.lambda_bedrock_logging.arn
+}
+
+# SQS Lambda 統合
+resource "aws_lambda_event_source_mapping" "mapping_for_sugoi_function" {
+  event_source_arn = aws_sqs_queue.bedrock_backend.arn
+  function_name    = aws_lambda_function.slack_bolt_app_bedrock_backend.arn
 }
---
+++ bedrock-slack-ai-chatbot/slack_bot_lambda/lambda_function.py
@@ -0,0 +1,43 @@
+from slack_bolt import App
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+import boto3
+import json
+import os
+import re
+
+sqs = boto3.client("sqs")
+
+aws_account_id = os.environ.get("ACCOUNT_ID")
+region = os.environ.get("REGION")
+queue = os.environ.get("BACKEND_QUEUE")
+
+sqs_queue_url = f"https://sqs.{region}.amazonaws.com/{aws_account_id}/{queue}"
+
+# アプリの初期化
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+    process_before_response=True,
+)
+
+# Slackイベントハンドラー：Slackアプリがメンションされた時
+@app.event("app_mention")
+def handle_app_mention_events(event, say):
+    result = say(text=f"少々お待ちください...")
+
+    channel_id = event["channel"]
+    input_text = re.sub("<@.+>", "", event["text"]).strip()
+
+    sqs.send_message(
+        QueueUrl=sqs_queue_url,
+        MessageBody=json.dumps({
+            "channel_id": channel_id,
+            "input_text": input_text,
+        }),
+    )
+
+
+# Lambdaイベントハンドラー
+def lambda_handler(event, context):
+    slack_handler = SlackRequestHandler(app=app)
+    return slack_handler.handle(event, context)
