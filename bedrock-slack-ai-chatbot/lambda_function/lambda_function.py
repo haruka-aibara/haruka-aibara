2a7c24b5e2cbf9772b5b6d0951a75cb3ecc2e2f6
haruharumolly <haruharumolly@outlook.com>
2024-08-31 11:45:24 +0900

    タイポ修正と不要なbedrock処理の削除

--- bedrock-slack-ai-chatbot/lambda_function/lambda_function.py
+++ bedrock-slack-ai-chatbot/lambda_function/lambda_function.py
@@ -7,11 +7,11 @@ import re
 
 sqs = boto3.client("sqs")
 
-aws_accoun_id = os.environ.get("ACCOUNT_ID")
+aws_account_id = os.environ.get("ACCOUNT_ID")
 region = os.environ.get("REGION")
 queue = os.environ.get("BACKEND_QUEUE")
 
-sqs_queue_url = f"https://sqs.{region}.amazonaws.com/{aws_account_id}/{queue_name}"
+sqs_queue_url = f"https://sqs.{region}.amazonaws.com/{aws_account_id}/{queue}"
 
 # アプリの初期化
 app = App(
@@ -19,38 +19,6 @@ app = App(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
     process_before_response=True,
 )
-
-# Bedrockを使って応答テキストを生成する
-def generate_answer(input_text):
-    # メッセージをセット
-    messages = [
-        {
-            "role": "user",
-            "content": input_text,
-        },
-    ]
-
-    # リクエストBODYをセット
-    request_body = json.dumps({
-        "messages": messages,
-        "anthropic_version": "bedrock-2023-05-31",
-        "max_tokens": 1000,
-    })
-
-    # Bedrock APIを呼び出す
-    response = bedrock_runtime.invoke_model(
-        modelId="anthropic.claude-3-haiku-20240307-v1:0",
-        accept="application/json",
-        contentType="application/json",
-        body=request_body,
-    )
-
-    # レスポンスBODYから応答テキストを取り出す
-    response_body = json.loads(response.get("body").read())
-    output_text = response_body.get("content")[0].get("text")
-
-    # 応答テキストを戻り値として返す
-    return output_text
 
 # Slackイベントハンドラー：Slackアプリがメンションされた時
 @app.event("app_mention")
@@ -67,6 +35,7 @@ def handle_app_mention_events(event, say):
             "input_text": input_text,
         }),
     )
+
 
 # Lambdaイベントハンドラー
 def lambda_handler(event, context):
