73f327cc4891b62ceda605394ddd0f2b1a8acaad
haruharumolly <haruharumolly@outlook.com>
2024-09-11 21:34:08 +0900

    use terraform cloud backend

--- bedrock-slack-ai-chatbot/terraform.tf
+++ bedrock-slack-ai-chatbot/terraform.tf
@@ -1,5 +1,12 @@
 # AWS プロバイダーは実装時のメジャーバージョンの最新以上を使用するよう定義
 terraform {
+  cloud {
+    organization = "aibara-ha"
+    workspaces {
+      name = "bedrock-slack-ai-chatbot"
+    }
+  }
+
   required_version = ">= 1.0.0"
 
   required_providers {
