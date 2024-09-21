3bc86b2e0f9266ca1ac67b8c9c28681fb78683b4
haruka-aibara <haruka_aibara@outlook.com>
2024-09-21 21:51:38 +0900

    use anthropic.claude-3-5-sonnet-20240620-v1:0 as default

--- modules/bedrock-agent-classic-slack/main.tf
+++ modules/bedrock-agent-classic-slack/main.tf
@@ -5,9 +5,9 @@ data "aws_partition" "current" {}
 data "aws_region" "current" {}
 
 resource "aws_bedrockagent_agent" "example" {
-  agent_name                  = "my-agent-name"
+  agent_name                  = var.agent_name
+  foundation_model            = var.bedrock_foundation_model_id
+  instruction                 = var.agent_instruction
   agent_resource_role_arn     = aws_iam_role.example.arn
   idle_session_ttl_in_seconds = 500
-  foundation_model            = "anthropic.claude-v2"
-  instruction                 = "質問内容に簡潔かつ的確に答えてください。不要な前置きは避け、ユーザーの意図を理解し、具体的で役立つ情報を提供することに集中してください。必要に応じて、追加の詳細や説明を提案することも可能です。"
 }
--- modules/bedrock-agent-classic-slack/variables.tf
+++ modules/bedrock-agent-classic-slack/variables.tf
@@ -7,3 +7,21 @@ variable "region" {
   type    = string
   default = "ap-northeast-1"
 }
+
+variable "bedrock_foundation_model_id" {
+  type        = string
+  description = "エージェントの基盤モデルID"
+  default     = "anthropic.claude-3-5-sonnet-20240620-v1:0"
+}
+
+variable "agent_name" {
+  type        = string
+  description = "エージェント名"
+  default     = "bedrock-slack-ai-agent"
+}
+
+variable "agent_instruction" {
+  type        = string
+  description = "エージェントが実行するタスクについて、明確かつ具体的な指示を提供します。特定のスタイルやトーンを指定することもできます。"
+  default     = "質問内容に簡潔かつ的確に答えてください。不要な前置きは避け、ユーザーの意図を理解し、具体的で役立つ情報を提供することに集中してください。必要に応じて、追加の詳細や説明を提案することも可能です。"
+}
