86c4050fff053587761156aab67cd1e4441945f9
haruka-aibara <haruka_aibara@outlook.com>
2024-10-05 21:32:02 +0900

    update required version

--- modules/generate-dev-io-summary/terraform.tf
+++ modules/generate-dev-io-summary/terraform.tf
@@ -8,12 +8,12 @@ terraform {
     }
   }
 
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.9.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.65.0"
     }
   }
 }
