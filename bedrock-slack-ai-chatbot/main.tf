87a2c100f0cf9e88921fa61a4cb4caaf53375449
haruka-aibara <haruka_aibara@outlook.com>
2024-10-21 21:56:08 +0900

    refactoring

--- bedrock-slack-ai-chatbot/main.tf
+++ bedrock-slack-ai-chatbot/main.tf
@@ -4,7 +4,7 @@ module "slack_bolt_app" {
   app_name             = local.project_name
   slack_bot_token      = var.slack_bot_token
   slack_signing_secret = var.slack_signing_secret
-  queue_arn            = module.sqs.queue_arn
+  queue_arn            = aws_sqs_queue.this.arn
 }
 
 module "bedrock_backend" {
@@ -13,13 +13,7 @@ module "bedrock_backend" {
   app_name             = local.project_name
   slack_bot_token      = var.slack_bot_token
   slack_signing_secret = var.slack_signing_secret
-  queue_arn            = module.sqs.queue_arn
-}
-
-module "sqs" {
-  source = "./modules/sqs"
-
-  name = local.project_name
+  queue_arn            = aws_sqs_queue.this.arn
 }
 
 module "api_gateway" {
