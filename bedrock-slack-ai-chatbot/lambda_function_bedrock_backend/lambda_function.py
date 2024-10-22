42945119d59cef2280bf8576908dd6e9efa11f51
haruka-aibara <haruka_aibara@outlook.com>
2024-10-23 06:20:56 +0900

    region

--- bedrock-slack-ai-chatbot/lambda_function_bedrock_backend/lambda_function.py
+++ bedrock-slack-ai-chatbot/lambda_function_bedrock_backend/lambda_function.py
@@ -9,8 +9,7 @@ from slack_sdk import WebClient
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-region = os.environ.get("AWS_REGION")
-bedrock_runtime = boto3.client("bedrock-runtime", region_name=region)
+bedrock_runtime = boto3.client("bedrock-runtime")
 
 client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 
