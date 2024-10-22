db895a562431583e5d0370eb4ac06e95cf2692ae
haruka-aibara <haruka_aibara@outlook.com>
2024-10-22 22:31:38 +0900

    dlq

--- bedrock-slack-ai-chatbot/main_sqs.tf
+++ bedrock-slack-ai-chatbot/main_sqs.tf
@@ -1,3 +1,21 @@
 resource "aws_sqs_queue" "slack_ai_chatbot" {
   name = "${local.project_name}-queue"
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.slack_ai_chatbot_dlq.arn
+    maxReceiveCount     = 1
+  })
+  message_retention_seconds = 60
+}
+
+resource "aws_sqs_queue" "slack_ai_chatbot_dlq" {
+  name = "${local.project_name}-dead-letter-queue"
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "slack_ai_chatbot_redrive_allow_policy" {
+  queue_url = aws_sqs_queue.slack_ai_chatbot_dlq.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.slack_ai_chatbot.arn]
+  })
 }
