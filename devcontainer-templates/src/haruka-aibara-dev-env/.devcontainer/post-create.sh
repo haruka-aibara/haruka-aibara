e2bc95c7b93ccf0201346e932b0f58a2a5ced4f0
haruka-aibara <haruka_aibara@outlook.com>
2025-05-03 23:00:07 +0000

    uv tools

--- devcontainer-templates/src/haruka-aibara-dev-env/.devcontainer/post-create.sh
+++ devcontainer-templates/src/haruka-aibara-dev-env/.devcontainer/post-create.sh
@@ -5,6 +5,10 @@ echo "Running post-create setup..."
 
 # uv tools
 uv tool install pycodestyle
+uv tool install flake8
+uv tool install pylint
+uv tool install pyre-check
+uv tool install pytest
 
 # Source .bashrc to apply changes
 source ~/.bashrc
