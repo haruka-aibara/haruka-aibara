b3ce4935afded12353ec788d61aecafc345409e1
haruka-aibara <haruka_aibara@outlook.com>
2025-05-04 11:47:11 +0000

    version 0.2.3

--- devcontainer-templates/src/haruka-aibara-dev-env/.devcontainer/post-create.sh
+++ devcontainer-templates/src/haruka-aibara-dev-env/.devcontainer/post-create.sh
@@ -3,13 +3,27 @@ set -e
 
 echo "Running post-create setup..."
 
-# uv tools
+# ======================================================================
+# Install uv tools
+# ======================================================================
+# code style
 uv tool install pycodestyle
 uv tool install flake8
 uv tool install pylint
+
+# Static typing
+# Usage: Check at the project directory level with commands like
+# uvx --from pyre-check pyre --source-directory "project_dir" check
 uv tool install pyre-check
+
+# Testing
+# Usage:
+# uvx pytest test_xxx.py -v
 uv tool install pytest
 
+# ======================================================================
+# tenv settings
+# ======================================================================
 # Install tenv for Terraform version management
 tenv tf install latest-stable
 
@@ -17,6 +31,11 @@ tenv tf install latest-stable
 echo '# tenv' >> ~/.bashrc
 echo 'export PATH=$(tenv update-path):$PATH' >> ~/.bashrc
 
+# ======================================================================
+# Use AWS icons in Markdown preview
+# ======================================================================
+# With this setup, you'll be able to use AWS icons in your Markdown preview, 
+# making your documentation more visually appealing and easier to understand when referencing AWS services.
 # Create the custom HTML head file with AWS icon support
 # reference article
 # https://qiita.com/take_me/items/83769d32c35e99b85ec8
