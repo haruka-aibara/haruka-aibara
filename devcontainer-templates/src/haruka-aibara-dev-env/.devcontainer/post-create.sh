2799a3ebfc9c2f9695ecca509cf784442817d767
haruka-aibara <haruka_aibara@outlook.com>
2025-05-04 03:03:12 +0000

    version 0.2.2

--- devcontainer-templates/src/haruka-aibara-dev-env/.devcontainer/post-create.sh
+++ devcontainer-templates/src/haruka-aibara-dev-env/.devcontainer/post-create.sh
@@ -18,6 +18,8 @@ echo '# tenv' >> ~/.bashrc
 echo 'export PATH=$(tenv update-path):$PATH' >> ~/.bashrc
 
 # Create the custom HTML head file with AWS icon support
+# reference article
+# https://qiita.com/take_me/items/83769d32c35e99b85ec8
 cat > ~/.local/state/crossnote/head.html << 'EOL'
 <!-- The content below will be included at the end of the <head> element. -->
 <script type="text/javascript">
