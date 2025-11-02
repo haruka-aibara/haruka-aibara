0a6c3d322cff8b7921112d25db1d052baa610503
haruka-aibara <haruka_aibara@outlook.com>
2025-11-02 07:02:22 +0000

    renovate

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,13 @@ terraform {
       name = "haruka-aibara"
     }
   }
+
+  required_version = "1.13.4"
+
+    required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.6"
+    }
+  }
 }
